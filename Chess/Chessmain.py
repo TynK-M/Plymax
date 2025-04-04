811066187fa07db340d86a7a6d7da06c40d9a671
TynK-M <164543445+TynK-M@users.noreply.github.com>
2025-04-04 15:16:39 +0200

    Switch to a bitboard representation

--- Chess/Chessmain.py
+++ Chess/Chessmain.py
@@ -1,28 +1,30 @@
 class GameState():
     def __init__(self):
         """
-        Code representation of the board.
-        First letter : color
-        ┣━━ b = Black
-        ┗━━ w = White
-        Second letter : piece
-        ┣━━ p = Pawn
-        ┣━━ Q = Queen
-        ┣━━ K = King
-        ┣━━ B = Bishop
-        ┣━━ N = Knight
-        ┗━━ R = Rook
+        Layout used
+        56 57 58 59 60 61 62 63
+        48 49 50 51 52 53 54 55
+        40 41 42 43 44 45 46 47
+        32 33 34 35 36 37 38 39
+        24 25 26 27 28 29 30 31
+        16 17 18 19 20 21 22 23
+        08 09 10 11 12 13 14 15
+        00 01 02 03 04 05 06 07
         """
-        self.board = [                       
-            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"] # 8
-            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"] # 7
-            ["--", "--", "--", "--", "--", "--", "--", "--"] # 6
-            ["--", "--", "--", "--", "--", "--", "--", "--"] # 5
-            ["--", "--", "--", "--", "--", "--", "--", "--"] # 4
-            ["--", "--", "--", "--", "--", "--", "--", "--"] # 3
-            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"] # 2  
-            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"] # 1
-            # a     b     c     d     e     f     g     h
-        ]
+        # White pieces (Capital letter)
+        self.P = 0b1111111100000000 # White pawn
+        self.R = 0b10000001 # White Rook
+        self.N = 0b1000010 # White Knight
+        self.B = 0b100100 # White Bishop
+        self.Q = 0b1000 # White Queen
+        self.K = 0b10000 # White King
+        # Black pieces (Small letter)
+        self.p = 0b11111111000000000000000000000000000000000000000000000000 # Black pawn
+        self.r = 0b1000000100000000000000000000000000000000000000000000000000000000 # Black Rook
+        self.n = 0b100001000000000000000000000000000000000000000000000000000000000 # Black Knight
+        self.b = 0b10010000000000000000000000000000000000000000000000000000000000 # Black Bishop
+        self.q = 0b100000000000000000000000000000000000000000000000000000000000 # Black Queen
+        self.k = 0b1000000000000000000000000000000000000000000000000000000000000 # Black King
+        
         self.whiteToMove = True # White always moves first
         self.moveLog = [] # Keep track of moves
